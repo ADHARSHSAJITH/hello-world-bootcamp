d1b128f9de2541c913a1072965edc3d5f15fe6b1
ADHARSHSAJITH <147493576+ADHARSHSAJITH@users.noreply.github.com>
2024-05-08 10:29:30 +0000

    day 1

---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int user_input;
+    printf("to start press enter\n");
+    printf("1=scissor,2=paper,3=rock\n");
+    printf("choose your choice: ");
+    scanf("%d",&user_input);
+    printf("uses choice is %d",user_input);
+    if(user_input==1)
+}
